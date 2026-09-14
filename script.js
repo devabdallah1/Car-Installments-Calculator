5fbfb54b42297f486243b6362cded8e8c6afd326
Abdallah Ahmed <abdallahahmed12mk@gmail.com>
2026-09-14 17:03:58 +0300

    v2

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ var DownPay= document.querySelector("#DownPay").value
   var loan =0;
  if (price == "" || DownPay==""|| period=="#") {
     document.querySelector('#output1').innerText= "Please complete all the fields"
+     document.querySelector('#output2').innerText= "Please complete all the fields"
  }
  else {
     if (period=="3") {
        loan = price - DownPay;
        var Total_Interest= loan * 0.08 *3;
-       var total = loan + Total_Interest;
-       var monthly = total / (3 * 12);
+       var total = parseFloat(loan ) + Total_Interest;
+       var monthly = parseFloat(total)  / (3 * 12);
       document.querySelector("#output1").innerText = monthly;
 document.querySelector("#output2").innerText = Total_Interest;
 
@@ -25,16 +26,16 @@ document.querySelector("#output2").innerText = Total_Interest;
     else if (period =='7') {
          loan = price - DownPay;
        var Total_Interest= loan * 0.12 * 7;
-       var total = loan + Total_Interest;
-       var monthly = total / (7 * 12);
+       var total = parseFloat(loan ) + Total_Interest;
+       var monthly = parseFloat(total) / (7 * 12);
       document.querySelector("#output1").innerText = monthly;
 document.querySelector("#output2").innerText = Total_Interest;
     }
     else if (period == '5') {
           loan = price - DownPay;
        var Total_Interest= loan * 0.10 * 5;;
-       var total = loan + Total_Interest;
-       var monthly = total / (5 * 12);
+       var total = parseFloat(loan ) + Total_Interest;
+       var monthly =parseFloat(total)  / (5 * 12);
       document.querySelector("#output1").innerText = monthly;
 document.querySelector("#output2").innerText = Total_Interest;
     }
